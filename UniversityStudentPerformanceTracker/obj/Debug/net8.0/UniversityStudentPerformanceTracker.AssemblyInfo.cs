289a208c045f812df6a4e4da38c447f808ad388f
NuwanjithS <nuwanjiths@fortude.co>
2024-06-23 10:58:55 +0530

    login

--- UniversityStudentPerformanceTracker/obj/Debug/net8.0/UniversityStudentPerformanceTracker.AssemblyInfo.cs
+++ UniversityStudentPerformanceTracker/obj/Debug/net8.0/UniversityStudentPerformanceTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UniversityStudentPerformanceTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39d2ec5e11f8d8570c779d05c7b931c292e56546")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d8cd77d6d1412ec29806b9fc892030f9b714a87")]
 [assembly: System.Reflection.AssemblyProductAttribute("UniversityStudentPerformanceTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UniversityStudentPerformanceTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
