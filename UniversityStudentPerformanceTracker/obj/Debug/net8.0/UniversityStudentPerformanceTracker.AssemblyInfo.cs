0128b716aa335844374c92864fa868ff97f29679
NuwanjithS <nuwanjiths@fortude.co>
2024-07-20 01:14:08 +0530

    Breaks also updated

--- UniversityStudentPerformanceTracker/obj/Debug/net8.0/UniversityStudentPerformanceTracker.AssemblyInfo.cs
+++ UniversityStudentPerformanceTracker/obj/Debug/net8.0/UniversityStudentPerformanceTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UniversityStudentPerformanceTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97f5fcbf9fa53f8c65f736b01c3d56c3b96b0689")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa57829f7ad0fd8017531565c30f2b39bfc891c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("UniversityStudentPerformanceTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UniversityStudentPerformanceTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
